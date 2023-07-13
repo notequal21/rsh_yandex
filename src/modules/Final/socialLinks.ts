a80cd6ff6183424a7c9a10c89a71a4a628b89e58
Kirill Makhnev <kalorigen@gmail.com>
2023-07-13 10:31:37 +0400

    updated shared links

--- src/modules/Final/socialLinks.ts
+++ src/modules/Final/socialLinks.ts
@@ -7,14 +7,14 @@ const currentUrl = window.location.origin + window.location.pathname;
 
 const socialLinks = [
   {
-    link: `https://vk.com/share.php?url=${currentUrl}&sn=VK`,
+    link: `https://vk.com/share.php?url=${currentUrl}?sn=VK`,
     icon: vkSsvg,
     sn: 'VK',
   },
-  { link: `tg://share?url=${currentUrl}&sn=TG`, icon: tgSvg, sn: 'TG' },
-  { link: `https://wa.me/?text=${currentUrl}&sn=WA`, icon: wuSvg, sn: 'WA' },
+  { link: `tg://share?url=${currentUrl}?sn=TG`, icon: tgSvg, sn: 'TG' },
+  { link: `https://wa.me/?text=${currentUrl}?sn=WA`, icon: wuSvg, sn: 'WA' },
   {
-    link: `https://connect.ok.ru/offer?url=${currentUrl}&sn=OK`,
+    link: `https://connect.ok.ru/offer?url=${currentUrl}?sn=OK`,
     icon: okSvg,
     sn: 'OK',
   },
